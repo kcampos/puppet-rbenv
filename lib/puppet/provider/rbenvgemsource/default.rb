1d43bef66a4618876e5a57435efa1b7fbb3d964d
Kyle Campos <kyle.campos@gmail.com>
2012-12-06 14:22:04 -0700

    Add rbenvgemsource type so we can manipulate gem sources

---
+++ lib/puppet/provider/rbenvgemsource/default.rb
@@ -0,0 +1,50 @@
+Puppet::Type.type(:rbenvgemsource).provide :default do
+  desc "Maintains gem sources inside an RBenv setup"
+
+  commands :su => 'su'
+
+  def add
+    args = ['--add']
+    args << "-v#{resource[:ensure]}" if !resource[:ensure].kind_of?(Symbol)
+    args << gem_source
+
+    output = gemsource(*args)
+    fail "Could not install: #{output.chomp}" if output.include?('ERROR')
+  end
+
+  def remove
+    gemsource '--remove', gem_source
+  end
+
+  def latest
+    @latest ||= list(:remote)
+  end
+
+  def current
+    list
+  end
+
+  private
+    def gem_source
+      resource[:gemsource]
+    end
+
+    def gemsource(*args)
+      exe =  "RBENV_VERSION=#{resource[:ruby]} " + resource[:rbenv] + '/bin/gem source'
+      su('-', resource[:user], '-c', [exe, *args].join(' '))
+    end
+
+    def list()
+      args = ['list']
+
+      gemsource(*args).lines.map do |line|
+        line =~ /^(?:\S+)\s+\((.+)\)/
+
+        return nil unless $1
+
+        # Fetch the version number
+        ver = $1.split(/,\s*/)
+        ver.empty? ? nil : ver
+      end.first
+    end
+end
